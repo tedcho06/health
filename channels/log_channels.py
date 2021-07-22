e0ee6c6257d26fec76ade7cb8823c3ead17a45c9
tedcho06 <86846685+tedcho06@users.noreply.github.com>
2021-07-22 23:29:28 +0900

    Add files via upload

---
+++ channels/log_channels.py
@@ -0,0 +1,16 @@
+log_add_success_channel = "867658521071058964"
+log_add_failure_channel = "867658521071058964"
+log_bot_start_channel = "867658521071058964"
+log_auto_self_check_failure_channel = "867658521071058964"
+log_auto_self_check_success_channel = "867658521071058964"
+log_auto_self_check_after_send_failure_channel ="867658521071058964"
+log_json_backup_channel = "867658521071058964"
+log_server_join = "867658521071058964"
+log_server_remove = "867658521071058964"
+log_add_remove = "867658521071058964"
+
+log_today = "867658521071058964"
+
+log_today_total = "867658924101206076"
+log_today_success = "867658521071058964"
+log_today_failure = "867658870700376084"
